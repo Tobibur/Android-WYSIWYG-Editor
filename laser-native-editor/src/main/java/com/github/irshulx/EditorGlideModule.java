29eb5789290208820f51e95f7da3346740001fb9
Tobibur <tobibur@eduaid.co.in>
2021-05-27 15:25:20 +0530

    removed glideapp module

--- laser-native-editor/src/main/java/com/github/irshulx/EditorGlideModule.java
+++ laser-native-editor/src/main/java/com/github/irshulx/EditorGlideModule.java
@@ -1,7 +1,5 @@
 package com.github.irshulx;
 
-import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.module.AppGlideModule;
-
+/*
 @GlideModule
-public final class EditorGlideModule extends AppGlideModule {}
+public final class EditorGlideModule extends AppGlideModule {}*/
