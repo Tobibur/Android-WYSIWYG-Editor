04b502d8607c68c2ac35232143f6f703d28f457e
Tobibur <tobibur@eduaid.co.in>
2021-05-27 14:54:22 +0530

    removed appmodule for glide

--- laser-native-editor/src/main/java/com/github/irshulx/EditorGlideModule.java
+++ laser-native-editor/src/main/java/com/github/irshulx/EditorGlideModule.java
@@ -3,5 +3,6 @@ package com.github.irshulx;
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.module.AppGlideModule;
 
+/*
 @GlideModule
-public final class EditorGlideModule extends AppGlideModule {}
+public final class EditorGlideModule extends AppGlideModule {}*/
